e003224f2d14f1fee99b13bbbeae3fd82f5afebf
Gracyele <gracyelesantossj@gmail.com>
2021-01-07 15:10:30 -0300

    feat: add the feeds components

--- lib/widgets/feed/menu_suspenso.dart
+++ lib/widgets/feed/menu_suspenso.dart
@@ -1,68 +1,100 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:provider/provider.dart';
+
+import 'package:ifs_network/managers/usuario_gerenciador.dart';
+import 'package:ifs_network/screens/login.dart';
+import 'package:ifs_network/screens/perfil.dart';
 
 class MenuSuspenso extends StatelessWidget {
+  Usuario usuario;
+
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: [
-        Column(children: [
-          IconButton(
-              icon: Icon(
-                Icons.access_alarm,
-                color: Theme.of(context).primaryColor,
-                size: 35,
+    UsuarioGerenciador usuarioGerenciador = Provider.of(context);
+
+    return SingleChildScrollView(
+      child: Consumer<UsuarioGerenciador>(
+        builder: (_, usuarioGerenciador, __) {
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Column(children: [
+                IconButton(
+                    icon: Icon(
+                      Icons.person,
+                      color: Theme.of(context).primaryColor,
+                      size: 35,
+                    ),
+                    onPressed: () async {
+                      usuario = await usuarioGerenciador.pegarUsuarioPorId(
+                          usuarioGerenciador.auth.currentUser.uid);
+                      Get.to(Perfil(
+                        usuario: usuario,
+                        id_usuario: usuarioGerenciador.auth.currentUser.uid,
+                      ));
+                    }),
+                Text("Perfil",
+                    style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                      fontSize: 14,
+                    ))
+              ]),
+              Column(children: [
+                IconButton(
+                    icon: Icon(
+                      Icons.notifications,
+                      color: Colors.teal[50], //Theme.of(context).primaryColor,
+                      size: 35,
+                    ),
+                    onPressed: null),
+                Text("Notificações",
+                    style: TextStyle(
+                      color: Colors.teal[50], //Theme.of(context).primaryColor,
+                      fontSize: 14,
+                    ))
+              ]),
+              Column(children: [
+                IconButton(
+                    icon: Icon(
+                      Icons.people,
+                      color: Colors.teal[50], //Theme.of(context).primaryColor,
+                      size: 35,
+                    ),
+                    onPressed: () {}),
+                Text("Clubes",
+                    style: TextStyle(
+                      color: Colors.teal[50], //Theme.of(context).primaryColor,
+                      fontSize: 14,
+                    ))
+              ]),
+              Column(
+                children: [
+                  IconButton(
+                      icon: Icon(
+                        Icons.exit_to_app,
+                        color: Theme.of(context).primaryColor,
+                        size: 35,
+                      ),
+                      onPressed: () {
+                        // Get.off(Login());
+                        Navigator.of(context)
+                            .push(MaterialPageRoute(builder: (ctx) => Login()));
+                        usuarioGerenciador.sair();
+                      }),
+                  Text(
+                    "Sair",
+                    style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                      fontSize: 14,
+                    ),
+                  )
+                ],
               ),
-              onPressed: null),
-          Text("Aff",
-              style: TextStyle(
-                color: Theme.of(context).primaryColor,
-                fontSize: 14,
-              ))
-        ]),
-        Column(children: [
-          IconButton(
-              icon: Icon(
-                Icons.access_alarm,
-                color: Theme.of(context).primaryColor,
-                size: 35,
-              ),
-              onPressed: null),
-          Text("Aff",
-              style: TextStyle(
-                color: Theme.of(context).primaryColor,
-                fontSize: 14,
-              ))
-        ]),
-        Column(children: [
-          IconButton(
-              icon: Icon(
-                Icons.access_alarm,
-                color: Theme.of(context).primaryColor,
-                size: 35,
-              ),
-              onPressed: null),
-          Text("Aff",
-              style: TextStyle(
-                color: Theme.of(context).primaryColor,
-                fontSize: 14,
-              ))
-        ]),
-        Column(children: [
-          IconButton(
-              icon: Icon(
-                Icons.access_alarm,
-                color: Theme.of(context).primaryColor,
-                size: 35,
-              ),
-              onPressed: null),
-          Text("Aff",
-              style: TextStyle(
-                color: Theme.of(context).primaryColor,
-                fontSize: 14,
-              ))
-        ]),
-      ],
+            ],
+          );
+        },
+      ),
     );
   }
 }
--- lib/widgets/feed/posts.dart
+++ lib/widgets/feed/posts.dart
@@ -1,15 +1,84 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
 
-class Posts extends StatelessWidget {
-  final String texto;
+import 'package:ifs_network/managers/comentarios_provider.dart';
+import 'package:ifs_network/managers/posts_provider.dart';
+import 'package:ifs_network/screens/comentarios.dart';
+import 'package:ifs_network/screens/perfil.dart';
+import 'package:ifs_network/widgets/feed/post_edicao.dart';
+import '../../managers/usuario_gerenciador.dart';
 
-  Posts(this.texto);
+class Posts extends StatefulWidget {
+  GlobalKey<ScaffoldState> scaffoldkey;
+  final Map<String, dynamic> postMap;
+  Usuario usuario;
+  String id_usuario;
+  var docPostagem;
+
+  Posts({this.usuario, this.postMap, this.docPostagem, this.scaffoldkey});
+
+  @override
+  _PostsState createState() => _PostsState();
+}
+
+class _PostsState extends State<Posts> {
+  bool _expanded = false;
+  CollectionReference postagens =
+      FirebaseFirestore.instance.collection('postagens');
+
+  final GlobalKey<FormState> formkey = GlobalKey<FormState>();
+  final TextEditingController textoController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+    Postagens postagem = Provider.of(context);
+    UsuarioGerenciador usuarioGerenciador = Provider.of(context);
+    String dataString = widget.postMap['data_postagem'] as String;
+    DateTime data = DateTime.parse(dataString);
+
+    showAlertDialog(BuildContext context) {
+      Widget cancelButton = FlatButton(
+        child: Text(
+          "Cancelar",
+          style: TextStyle(color: Colors.grey),
+        ),
+        onPressed: () {
+          Navigator.of(context).pop();
+        },
+      );
+      Widget continueButton = FlatButton(
+        color: Colors.red,
+        child: Text("Excluir"),
+        onPressed: () {
+          postagens.doc(widget.docPostagem).delete();
+          Navigator.of(context).pop();
+        },
+      );
+
+      AlertDialog alert = AlertDialog(
+        actionsPadding: EdgeInsets.fromLTRB(10, 10, 30, 10),
+        title: Text("Tem certeza que deseja excluir essa publicação?"),
+        actions: [
+          cancelButton,
+          continueButton,
+        ],
+      );
+
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return alert;
+        },
+      );
+    }
+
+    String dataPostagem = DateFormat('dd-MM-yyyy – kk:mm').format(data);
+
     return Card(
       color: Colors.white,
-      elevation: 2,
       margin: EdgeInsets.fromLTRB(10, 10, 10, 20),
       shadowColor: Colors.grey[50],
       child: Column(
@@ -23,65 +92,185 @@ class Posts extends StatelessWidget {
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Row(
-                        children: [
-                          Container(
-                            margin: EdgeInsets.only(left: 10),
-                            child: CircleAvatar(
-                              backgroundImage: NetworkImage(
-                                  'https://www.socialismocriativo.com.br/wp-content/uploads/2020/06/bob-esponja-calca-quadrada-nickelodeon-viacomcbs-3778837.jpg'),
+                      GestureDetector(
+                        onTap: () {
+                          Get.to(Perfil(
+                              usuario: widget.usuario,
+                              id_usuario: widget.postMap['id_usuario']));
+                        },
+                        child: Row(
+                          children: [
+                            Container(
+                              margin: EdgeInsets.only(left: 10),
+                              child: CircleAvatar(
+                                backgroundImage:
+                                    NetworkImage(widget.usuario.imagem),
+                              ),
                             ),
-                          ),
-                          Container(
-                            margin: EdgeInsets.only(left: 10),
-                            child: Text('Bob Esponja'),
-                          )
-                        ],
+                            Container(
+                              margin: EdgeInsets.only(left: 10),
+                              child: Text(widget.usuario.nome),
+                            )
+                          ],
+                        ),
                       ),
-                      IconButton(icon: Icon(Icons.more_vert), onPressed: null)
+                      Text(
+                        dataPostagem,
+                        style: TextStyle(color: Colors.grey, fontSize: 10),
+                      ),
+                      DropdownButton<String>(
+                        icon: Icon(Icons.more_vert),
+                        iconSize: 24,
+                        elevation: 2,
+                        style: TextStyle(color: Colors.teal),
+                        underline: Container(
+                          height: 0,
+                        ),
+                        onChanged: (String newValue) {
+                          setState(() {
+                            if (newValue.compareTo('Excluir') == 0) {
+                              showAlertDialog(context);
+                            } else {
+                              print("erro!");
+                            }
+                            if (newValue.compareTo('Editar') == 0) {
+                              Get.to(PostEdicao(
+                                docPostagem: widget.docPostagem,
+                                postMap: widget.postMap,
+                                scaffoldkey: widget.scaffoldkey,
+                                usuario: widget.usuario,
+                              ));
+                            }
+
+                            if (newValue.compareTo('Denunciar') == 0) {
+                              //TODO: denunciar publicacao
+                            }
+                          });
+                        },
+                        items: widget.usuario.id ==
+                                usuarioGerenciador.auth.currentUser.uid
+                            ? <String>['Editar', 'Excluir', 'Denunciar']
+                                .map<DropdownMenuItem<String>>((String value) {
+                                return DropdownMenuItem<String>(
+                                  value: value,
+                                  child: Text(value),
+                                );
+                              }).toList()
+                            : <String>['Denunciar']
+                                .map<DropdownMenuItem<String>>((String value) {
+                                return DropdownMenuItem<String>(
+                                  value: value,
+                                  child: Text(value),
+                                );
+                              }).toList(),
+                      )
                     ],
                   ),
                 ),
                 Container(
                   margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                  child: Column(
-                    children: [
-                      Align(
-                        alignment: Alignment.bottomLeft,
-                        child: Text(
-                          'Happy birthday to me!!',
-                          style: TextStyle(fontSize: 16),
-                        ),
+                  child: Column(children: [
+                    Align(
+                      alignment: Alignment.bottomLeft,
+                      child: Text(
+                        widget.postMap['texto'],
+                        style: TextStyle(fontSize: 16),
                       ),
-                      Container(
-                          margin: EdgeInsets.only(top: 10),
-                          width: double.infinity,
-                          child: Image.network(
-                              'https://i1.wp.com/telaviva.com.br/wp-content/uploads/2020/01/bob-esponja--scaled.jpg?resize=450%2C300&ssl=1')),
-                    ],
-                  ),
-                )
-              ],
-            ),
-          ),
-          Container(
-            child: Row(
-              children: [
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 10),
+                      width: double.infinity,
+                      child: Image.network(widget.postMap['imagem']),
+                    )
+                  ]),
+                ),
+                Divider(),
                 Container(
-                  margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                  width: 270,
-                  height: 33,
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                      hintText: 'Comentar',
-                      hintStyle: TextStyle(
-                        color: Colors.grey[300],
-                      )
+                  margin: EdgeInsets.only(left: 10),
+                  child: Align(
+                    alignment: AlignmentDirectional.topCenter,
+                    child: GestureDetector(
+                      child: Text(
+                        'ver comentários',
+                        style: TextStyle(color: Colors.teal, fontSize: 12),
+                      ),
+                      onTap: () {
+                        Get.to(
+                            ComentariosScreen(docPostagem: widget.docPostagem));
+                      },
                     ),
                   ),
                 ),
-                IconButton(
-                    icon: Icon(Icons.message_outlined, color: Colors.teal,), onPressed: () {}),
+                Divider(),
+                Form(
+                  key: formkey,
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Container(
+                          height: _expanded ? 100 : 40,
+                          padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                          child: TextFormField(
+                            maxLength: 500,
+                            expands: true,
+                            maxLines: null,
+                            minLines: null,
+                            keyboardType: TextInputType.multiline,
+                            maxLengthEnforced: true,
+                            cursorColor: Colors.teal,
+                            onTap: () {
+                              setState(() {
+                                _expanded = true;
+                              });
+                            },
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: 'Adicione um comentário',
+                              counterText: '',
+                              hintStyle: TextStyle(
+                                  color: Colors.grey[400], fontSize: 14),
+                            ),
+                            controller: textoController,
+                          ),
+                        ),
+                      ),
+                      IconButton(
+                        icon: Icon(
+                          Icons.send,
+                          color: Colors.teal,
+                        ),
+                        onPressed: () {
+                          try {
+                            if (formkey.currentState.validate()) {
+                              context.read<Comentarios>().comentar(Comentario(
+                                    texto: textoController.text,
+                                    data: DateTime.now().toIso8601String(),
+                                    id_postagem: widget.docPostagem,
+                                    id_autor:
+                                        usuarioGerenciador.auth.currentUser.uid,
+                                  ));
+
+                              textoController.text = "";
+                            }
+                            widget.scaffoldkey.currentState
+                                .showSnackBar(SnackBar(
+                              content: const Text('Comentario feito!'),
+                              backgroundColor: Colors.green,
+                              duration: Duration(milliseconds: 600),
+                            ));
+                            print("SUCESSSOOO");
+                          } catch (e) {
+                            print("ERROOOOOOOO: $e");
+                          }
+
+                          setState(() {
+                            _expanded = !_expanded;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                )
               ],
             ),
           ),
